3fa61799e5202f46e6cb2d4d3132314a4bd77530
97Mith <136820655+97Mith@users.noreply.github.com>
2024-04-22 19:12:03 -0300

    Criado findAll e findById na camada repository

--- src/main/java/org/example/repository/CompanyRepository.java
+++ src/main/java/org/example/repository/CompanyRepository.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.swing.*;
+import java.util.List;
 
 public class CompanyRepository {
     public static void createCompany(CompanyEntity company){
@@ -27,6 +28,30 @@ public class CompanyRepository {
                     null, "Ocorreu um erro ao se conectar ao banco de dados.",
                     "Aviso", JOptionPane.ERROR_MESSAGE
             );
+        }
+    }
+    public static CompanyEntity findById(Integer id) {
+
+        try {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("unit-jpa");
+            EntityManager em = emf.createEntityManager();
+
+            return em.find(CompanyEntity.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static List<CompanyEntity> findAll() {
+        try {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("unit-jpa");
+            EntityManager em = emf.createEntityManager();
+
+            return em.createQuery("SELECT c FROM CompanyEntity c" , CompanyEntity.class).getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
